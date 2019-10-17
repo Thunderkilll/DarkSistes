4827ca7652cdd124fe577c77fefdae0372838496
Khaled_guesmi <khaled.guesmi@esprit.tn>
2019-10-17 04:15:25 +0200

    weapons
    i added the possibility to add a weapon an equip it
    a sound when i jump
    and deleted Debug.Log lines that take away some of our game's performence

--- Assets/DarkSisters/Code/Scripts/Player/PlayerController.cs
+++ Assets/DarkSisters/Code/Scripts/Player/PlayerController.cs
@@ -9,6 +9,8 @@ public class PlayerController : MonoBehaviour
     public float jump;
     public int numberOfJumps;
     private int numberOfJumpsValue;
+    public AudioSource audio;
+    public GameObject prefab;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -18,59 +20,92 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Time.timeScale == 0f)
-        {
-           if (Input.GetKeyDown(KeyCode.M)) {
-                
-                gameObject.transform.position = new  Vector3(0, 1, 0);
-                Debug.Log(gameObject.transform.position);
-                Time.timeScale = 1f;
-            }
-            
-        }
-        if (Input.GetKey(KeyCode.Z))
-        {
-            rb.AddForce(Vector3.forward * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
-            print("Z");
-        }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rb.AddForce(Vector3.left * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
-            print("Q");
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(Vector3.back * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
-            print("S");
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(Vector3.right * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
-            print("D");
-        }
-        if (Input.GetKeyDown(KeyCode.Space) && (numberOfJumps != 0))
-        {
-            rb.AddForce(Vector3.up * jump * 50 * Time.deltaTime, ForceMode.Impulse);
-            print("Jumpe");
-            numberOfJumps--;
-        }
-
-
+        resetPlayer();
+        //MovementControlls();
+        
     }
 
     void OnCollisionEnter(Collision collision)
     {
+        //touching the ground 
         if (collision.gameObject.tag == "Ground")
         {
             numberOfJumps = numberOfJumpsValue;
-            print("collided");
+            
         }
+        // upper boundaries == death
         if (collision.gameObject.tag == "boundaries")
         {
             Time.timeScale = 0f;
             Debug.Log("game over for player / respawn");
            
         }
+        // picking up a sword 
+        if (collision.gameObject.tag == "Weapon")
+        {
+            Destroy(collision.gameObject); //destroy the new weapon 
+            GameObject weaponHolder =  gameObject.transform.GetChild(0).gameObject ; // get the weaponholder
+            Vector3 offset = new Vector3(.5f,0,-4f); //just an offset for a bug that need to be fixed 
+            GameObject o = Instantiate(prefab, weaponHolder.transform.position + offset , Quaternion.identity); //initiate the new weapon in our hands 
+            o.transform.SetParent(weaponHolder.transform); //set weapon holder as parent
+            
+        }
+
+    }
+
+    //reset player life 
+    public void resetPlayer(){
+
+        if (Time.timeScale == 0f)
+        {
+            if (Input.GetKeyDown(KeyCode.M))
+            {
+
+                gameObject.transform.position = new Vector3(0, 1, 0);
+                Debug.Log(gameObject.transform.position);
+                Time.timeScale = 1f;
+            }
+
+        }
+        else
+        {
+            MovementControlls();
+        }
+    }
+
+
+    //movement code
+    public void MovementControlls()
+    {
+        if (Input.GetKey(KeyCode.Z))
+        {
+            rb.AddForce(Vector3.forward * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
+
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rb.AddForce(Vector3.left * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
+
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(Vector3.back * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
+
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(Vector3.right * speed * 50 * Time.deltaTime, ForceMode.Acceleration);
+
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && (numberOfJumps != 0))
+        {
+
+            audio.Play();
+            rb.AddForce(Vector3.up * jump * 50 * Time.deltaTime, ForceMode.Impulse);
+
+            numberOfJumps--;
+        }
+
 
     }
 }
